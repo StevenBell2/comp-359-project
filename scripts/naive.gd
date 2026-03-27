f1b5341796b3199a250b6bc27a634f92239df5fe
Dilpreet Gill <dilpreetgill3537@gmail.com>
2026-03-27 10:50:43 -0700

    created naive solution

--- scripts/naive.gd
+++ scripts/naive.gd
@@ -1,1 +1,14 @@
-extends Node
+class_name Naive
+
+
+func get_nearby(entity, radius, entities) -> Array:
+	var result = []
+	
+	for e in entities:
+		if e == entity:
+			continue
+		
+		if(entity.position.distance_to(e.position) < (radius + e.radius)):
+			result.append(e)
+	
+	return result
